510d9e32f5b2442b8e8c73f61a7c30e8440c854a
biovino <biovino@gmail.com>
2022-04-13 15:08:57 -0400

    driver object initialized

--- insert_database.py
+++ insert_database.py
@@ -6,7 +6,6 @@ Ben Iovino  4/12/22   RunSQLight
 ===================================================================================================================="""
 
 import sqlite3 as sq3
-import beautifulsoup
 import selenium
 
 
@@ -16,6 +15,13 @@ def get_weather(zipcode):
 
     :param zipcode: 5 digit number i.e. 47906
     =============================================================================================================="""
+
+    driver = webdriver.Firefox()
+    driver.get('https://www.wunderground.com')
+
+    search_bar = driver.find_element_by_id('wuSearch')
+    search_bar.sendKeys(zipcode)
+
 
 def get_input():
     """=================================================================================================================
@@ -27,12 +33,12 @@ def get_input():
     flag = True
     while flag == True:
         datetime = input('Enter date and time (i.e. 2022-01-01 12:00 AM): ')
-        type = input('Enter type of run: ')
-        distance = float(input('Enter distance of run: '))
-        duration = input('Enter duration of run (i.e. HH:MM:SS): ')
+        #type = input('Enter type of run: ')
+        #distance = float(input('Enter distance of run: '))
+        #duration = input('Enter duration of run (i.e. HH:MM:SS): ')
         zipcode = input('Enter zipcode to get weather: ')
-        notes = input('Notes: ')
-
+        #notes = input('Notes: ')
+        '''
         # Break loop if user types Y, continue if N
         print()
         input_flag = input('Does everything look correct? (Y/N): ')
@@ -48,13 +54,17 @@ def get_input():
     pace_min = pace_sec // 60
     pace_sec = pace_sec % 60
     pace = f'{int(pace_min)}:{int(pace_sec)}'
+    '''
 
     # Scrape weather by putting zipcode into get_weather()
+    get_weather(zipcode)
 
+    '''
     # Return input
     input_tuple = [datetime, type, distance, duration, pace, notes]
     print(input_tuple)
     return input_tuple
+    '''
 
 
 def main():
